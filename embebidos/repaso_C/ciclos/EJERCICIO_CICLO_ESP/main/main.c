7ceda2fdb6438dabb9dc2b23b76e56613e4bcc20
dmrx <dmrx@gmail.com>
2025-11-01 18:20:09 -0700

    ejercicios cicos v2

--- embebidos/repaso_C/ciclos/EJERCICIO_CICLO_ESP/main/main.c
+++ embebidos/repaso_C/ciclos/EJERCICIO_CICLO_ESP/main/main.c
@@ -17,13 +17,14 @@ factorial es la multiplicacion de los numeros enteros e incluso el mismo numero
 */
 
 
+//factorual ya esta chilo 
 int factorial(int intervalo){
 
     int suma = 1;
-    for(int i=0; i < intervalo ; i++){
+    for(int i=1; i <= intervalo ; i++){
 
         //un facotural es la suma de todos sus valores anteriores hasta el
-        suma += i;
+        suma *= i;
     }
 
     return suma;
@@ -40,19 +41,47 @@ void scale(int numer_scaled){
 
 void app_main(void)
 {
-    int euler =1; 
+    int euler =0;
+    int sumador = 0; 
 
-    for(int i=0 ; i < 10; i++){
+    int i;
+    int j;
+    for(i=0 ; i < 10; i++){
 
-        for(int j=1; j< 10 && j<=i;j++){
+        printf("%d -> ", i);
+        for(j=0; j< 10 && j<=i;j++){
 
-            int fac = factorial(j);
-            euler += SCALE/euler;
-            printf(" - ");
-            scale(euler);
-            printf(" - ");
+            // if(j==1){
+            //     continue;
+            // }
+            // else{
+            //     int fac = factorial(j);
+            //     euler = euler + (SCALE /fac);
+            //     printf(" - ");
+            //     scale(euler);
+            //     printf("%d",euler);
+            //     printf(" - ");
+            // }
+            
+
+            
+            // // printf("- %d -", fac);
+            // sumador = SCALE/fac;
+            // euler +=sumador;
+            // printf(" - ");
+            // scale(euler);
+            // printf("%d",euler);
+            // printf(" - ");
         }
 
+
+        int fac = factorial(j);
+        euler += 1 + (SCALE/fac);
+
+        printf(" - ");
+        scale(euler);
+        printf("%d",euler);
+        printf(" - ");
         printf("\n");
 
     }
